f1637bcad6bf3c2e04fac45713ef564d4ace67be
ovi <eu_oni@yahoo.com>
2026-04-16 17:12:20 +0300

    Add book function and Book.css

--- src/components/BookForm.tsx
+++ src/components/BookForm.tsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 import type { Book } from "../components/types";
 
+import styles from "../components/Book.module.css"
+
+
 
 const apiUrl = `${import.meta.env.VITE_API_URL}/books`;
 
@@ -22,22 +25,60 @@ export function Books() {
 
 
 
+async function addBookToDB(e: React.SubmitEvent<HTMLFormElement>) {
+    e.preventDefault();
+    if(!book) return;
+    
+       const form = e.target;
+        const data = new FormData(form);
+        const title = data.get("title");
+        const author = data.get("author");
+        
+
+   const newBook = await fetch(apiUrl,
+ {
+    method:"POST",
+    headers:{
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ title, author, isRead: false }),
+ }). then((response) => response.json());
+
+ setBooks([...book, newBook])
+
+}
     return (
         <>
             <p>test</p>
             <div>
                 {book.map((m) => (
-                    <ul>
-                        <li>key={m.id}</li>
-                        <li>title={m.title}</li>
-                        <li>author={m.author}</li>
-                        <li>read status={m.isRead}</li>
+                    <ul>key={m.id}
+                        
+                        <li>title:{m.title}</li>
+                        <li>author:{m.author}</li>
+                        <li>read status:{m.isRead}</li>
 
 
                     </ul>
                 )
                 )}
+                <button onClick={toogleaddBooks} className={styles.button}>
+                    {addBook ? "Back" : "Add Book"}
+                </button>
             </div>
+
+
+             {
+                    addBook && (
+                        <form onSubmit={addBookToDB} className={styles.form}>
+                            <input name="title" placeholder="Book title" />
+                            <input name="author" placeholder="author" />
+                            <button type="submit" className={styles.button}>
+                                Add
+                            </button>
+                        </form>
+                    )
+                }
         </>
     )
 }
